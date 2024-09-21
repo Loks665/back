cbbd4742c2b6e3b1d249d44943d0d7cf03788a54
Loks665 <nik270518@mail.ru>
2024-09-21 12:54:32 +0300

    addip

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const app = express();
+//app.use(cors());
+const fs = require('fs'); 
+const path = require('path');
+const localhost = '172.31.1.203';
 const port = 3000;
+
+
+
+function read_data(path = dataPath) {
+  try {
+      const data = fs.readFileSync(path, 'utf-8'); 
+      if (data == '') {
+          return {};
+      }
+      const parsedData = JSON.parse(data);
+      return parsedData;
+  } catch (err) {
+      return err.code === 'ENOENT' ? {} : (() => { throw err })();
+  }
+}
+
+function write_data(newData, path = dataPath) {
+  const data = { ...read_data(path), ...newData };
+  fs.writeFileSync(path, JSON.stringify(data, null, 4), 'utf-8');
+}
+
+
 
 // Обработка запросов на главную страницу
 app.get('/', (req, res) => {
-  res.send('Привет, мир!');
+  res.send('Привет, fsdhsdfh!');
 });
 
 // Обработка запросов на страницу "/about"
@@ -12,7 +38,10 @@ app.get('/about', (req, res) => {
   res.send('Это страница "О нас".');
 });
 
+
+
+
 // Запуск сервера
 app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`);
+  console.log(`Сервер запущен на http://${localhost}:${port}`);
 });
